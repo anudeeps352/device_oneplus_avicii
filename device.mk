7811061a2cbc51b2339e4e1767e3c483bfc2b02f
mustangs066 <anudeeps352@gmail.com>
2021-12-05 20:48:17 +0530

    added device_oenplus_avicii

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ $(call inherit-product-if-exists, vendor/oneplus/apps/avicii/config.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-revengeos
 
 PRODUCT_PACKAGES += \
     OnePlusIconShapeCircleOverlay \
--- revengeos_avicii.mk
+++ revengeos_avicii.mk
@@ -22,15 +22,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from avicii device
 $(call inherit-product, device/oneplus/avicii/device.mk)
 
-# Inherit some common AospExtended stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common Revengeos stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
-# Aosp Extended Properties
+# Revengeos Properties
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_AOSP_RECOVERY := true
 EXTRA_FOD_ANIMATIONS := true
 
-PRODUCT_NAME := aosp_avicii
+PRODUCT_NAME := revengeos_avicii
 PRODUCT_DEVICE := avicii
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
